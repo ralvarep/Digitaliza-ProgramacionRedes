f11bbbe0fe3685a72c178ceffce852a1fee10b6b
Raúl Álvarez <ralvarep@users.noreply.github.com>
2019-02-16 13:42:59 +0100

    Create 03_personal-info.py

---
+++ Python/03_personal-info.py
@@ -0,0 +1,7 @@
+nombre=input("Nombre: ")
+apellido=input("Apellido: ")
+telefono=input("Teléfono: ")
+ciudad=input("Ciudad: ")
+edad=input("Edad: ")
+
+print("La persona " + nombre + " " + apellido + ", de " + edad + " años, vive en " + ciudad + " y tiene el teléfono " + telefono + ".")
