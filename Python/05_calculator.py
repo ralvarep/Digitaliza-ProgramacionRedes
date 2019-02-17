a7727fe1b8112849f8ed20debf12ffc5169a103f
Raúl Álvarez <ralvarep@users.noreply.github.com>
2019-02-17 23:36:33 +0100

    Update 05_calculator.py

--- Python/05_calculator.py
+++ Python/05_calculator.py
@@ -1,5 +1,15 @@
 import math
 
+def input_1():
+    global num1
+    num1=int(input(" Número_1: "))
+    
+def input_2():
+    global num1
+    global num2
+    num1=int(input(" Número_1: "))
+    num2=int(input(" Número_2: "))
+    
 def suma(num1,num2):
     return num1+num2
 
@@ -12,23 +22,41 @@ def multiplicacion(num1,num2):
 def division(num1,num2):
     return num1/num2
 
-def exponencial(num1,num2):
-    return math.expnum1^num2 
+def exponencial(num1):
+    return math.exp(num1)
 
-num1=int(input("Número 1: "))
-num2=int(input("Número 2: "))
+operaciones = {"1":"Suma",
+              "2":"Resta",
+              "3":"Multiplicación",
+              "4":"Divisón",
+              "5":"Exponencial"}
 
-operacion=(input("¿Qué operación quiere realizar? (+,-,*,/,^): "))
+print("¿Qué operación quiere realizar?")
 
-if operacion == "+":
-    print(str(num1) + " + " + str(num2) + " = " + str(suma(num1,num2)))
-elif operacion == "-":
-    print(str(num1) + " - " + str(num2) + " = " + str(resta(num1,num2)))
-elif operacion == "*":
-    print(str(num1) + " * " + str(num2) + " = " + str(multiplicacion(num1,num2)))
-elif operacion == "/":
-    print(str(num1) + " / " + str(num2) + " = " + str(division(num1,num2)))
-elif operacion == "^":
-    print(str(num1) + " ^ " + str(num2) + " = " + str(exponencial(num1,num2)))
+for operacion in operaciones:
+    print(" " + operacion + ") " + operaciones[operacion])
+    
+operacion=(input("Seleccione una opción: "))
+
+if operacion == "1":
+    print("\nSUMA: Número_1 + Número_2")
+    input_2()
+    print(" => " + str(num1) + " + " + str(num2) + " = " + str(suma(num1,num2)))
+elif operacion == "2":
+    print("\nRESTA: Número_1 - Número_2")
+    input_2()
+    print(" => " + str(num1) + " - " + str(num2) + " = " + str(resta(num1,num2)))
+elif operacion == "3":
+    print("\nMULTIPLICACIÓN: Número_1 * Número_2")
+    input_2()
+    print(" => " + str(num1) + " * " + str(num2) + " = " + str(multiplicacion(num1,num2)))
+elif operacion == "4":
+    print("\nDIVISIÓN: Número_1 / Número_2")
+    input_2()
+    print(" => " + str(num1) + " / " + str(num2) + " = " + str(division(num1,num2)))
+elif operacion == "5":
+    print("\nEXPONENCIAL: e ^ Número_1")
+    input_1()
+    print(" => e ^ " + str(num1) + " = " + str(exponencial(num1)))
 else:
-    print("Operación desconocida")
+    print("\nOPERACIÓN DESCONOCIDA")
