74fee64e4a6c46831e7d3146f0c22d4c01037b63
Raúl Álvarez <ralvarep@users.noreply.github.com>
2019-04-02 17:40:33 +0200

    Update 05_calculator.py

--- Python/05_calculator.py
+++ Python/05_calculator.py
@@ -1,15 +1,9 @@
-import math
-
-def input_1():
-    global num1
-    num1=int(input(" Número_1: "))
-    
-def input_2():
+def input_nums():
     global num1
     global num2
     num1=int(input(" Número_1: "))
     num2=int(input(" Número_2: "))
-    
+
 def suma(num1,num2):
     return num1+num2
 
@@ -22,8 +16,8 @@ def multiplicacion(num1,num2):
 def division(num1,num2):
     return num1/num2
 
-def exponencial(num1):
-    return math.exp(num1)
+def exponencial(num1,num2):
+    return num1**num2
 
 operaciones = {"1":"Suma",
               "2":"Resta",
@@ -32,32 +26,31 @@ operaciones = {"1":"Suma",
               "5":"Exponencial"}
 
 print("¿Qué operación quiere realizar?")
-
 for operacion in operaciones:
     print(" " + operacion + ") " + operaciones[operacion])
-    
 operacion=(input("Seleccione una opción: "))
 
 try:
     if operacion == "1":
         print("\nSUMA: Número_1 + Número_2")
-        input_2()
+        input_nums()
         print(" => " + str(num1) + " + " + str(num2) + " = " + str(suma(num1,num2)))
     elif operacion == "2":
         print("\nRESTA: Número_1 - Número_2")
-        input_2()
+        input_nums()
         print(" => " + str(num1) + " - " + str(num2) + " = " + str(resta(num1,num2)))
     elif operacion == "3":
         print("\nMULTIPLICACIÓN: Número_1 * Número_2")
-        input_2()
+        input_nums()
         print(" => " + str(num1) + " * " + str(num2) + " = " + str(multiplicacion(num1,num2)))
     elif operacion == "4":
         print("\nDIVISIÓN: Número_1 / Número_2")
-        input_2()
+        input_nums()
         print(" => " + str(num1) + " / " + str(num2) + " = " + str(division(num1,num2)))
     elif operacion == "5":
-        print("\nEXPONENCIAL: e ^ Número_1")
-        input_1()
-        print(" => e ^ " + str(num1) + " = " + str(exponencial(num1))) 
-except:
-    print("\n<< OPERACIÓN DESCONOCIDA >>")
+        print("\nEXPONENCIAL: Número_1 ^ Número_2")
+        input_nums()
+        print(" => " + str(num1) + " ^ " + str(num2) + " = " + str(exponencial(num1,num2)))
+
+except Exception as e:
+    print("\n<<< OPERACIÓN DESCONOCIDA (" + str(e) + ") >>>")
